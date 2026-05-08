6fa98a82e98117ee49eefe0c319a7eb9a4caae7f
farelhalim28 <farel24si@mahasiswa.pcr.ac.id>
2026-05-08 16:17:37 +0700

    Pertemuan 7

---
+++ src/components/Loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-2xl font-bold">Loading...</h1>
+    </div>
+  );
+}
